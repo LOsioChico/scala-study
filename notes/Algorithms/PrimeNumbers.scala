7a35b609493b0086e06af6d33f8c2bad8fd96a48
Luis Osio Chico <losio@unal.edu.co>
2024-06-07 22:38:31 -0500

    style: limit number of printed prime numbers to 15

--- notes/Algorithms/PrimeNumbers.scala
+++ notes/Algorithms/PrimeNumbers.scala
@@ -32,6 +32,6 @@ object PrimeNumbers extends App {
       val primesLowerThanNumSqrt = primes.takeWhile(_ <= math.sqrt(num))
       !primesLowerThanNumSqrt.exists(prime => num % prime == 0)
 
-  println(primes.take(500000).toList)
+  println(primes.take(15).toList)
   // List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
 }
