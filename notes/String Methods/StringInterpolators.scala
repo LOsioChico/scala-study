db35e9f3fb3f6e9ef24d8cc99b474ca00ee317b5
Luis Osio Chico <losio@unal.edu.co>
2024-06-15 20:18:10 -0500

    feat: Add StringInterpolators.scala for string interpolation

---
+++ notes/String Methods/StringInterpolators.scala
@@ -0,0 +1,21 @@
+//> using scala "3.3.1"
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * String Interpolators are a way to embed expressions in a string. We can use
+ * the s interpolator to embed expressions in a string. We can also use the f
+ * interpolator to format numbers in a string. The raw interpolator is used to
+ * avoid escaping characters in a string.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Reference:
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+object StringInterpolators extends App {
+  println(s"The answer is ${6 * 7}.") // <- The answer is 42.
+
+  println(raw"No\\\\escape!") // <- No\\\\escape!
+
+  println(f"${math.Pi}%.5f") // <- 3.14159
+}
