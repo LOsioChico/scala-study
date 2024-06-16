23c447ec0c309c8ad5dc3a88f4cc1b491e240b71
Luis Osio Chico <losio@unal.edu.co>
2024-06-15 20:08:09 -0500

    feat: Add StripMargin.scala for formatting multiline strings

---
+++ notes/String Methods/StripMargin.scala
@@ -0,0 +1,33 @@
+//> using scala "3.3.1"
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * The stripMargin method is a method that allows us to format multiline strings
+ * in a more readable way. It removes the leading spaces in each line of the
+ * string. The default character used to remove the spaces is the pipe character
+ * (|), but we can specify a different character if we want.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Reference: Programming in Scala, Fifth Edition by Martin Odersky
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+object StripMargin extends App {
+  val text = """This is a multiline text
+                that we can format
+                using the stripMargin method."""
+
+  println(text)
+  // This is a multiline text
+  //               that we can format
+  //               using the stripMargin method.
+
+  val textFormatted = """This is a multiline text
+                        |that we can format
+                        |using the stripMargin method.""".stripMargin
+
+  println(textFormatted)
+  // This is a multiline text
+  // that we can format
+  // using the stripMargin method.
+}
