15f17c21e7f2982a33023e93bd1a92fea8394394
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-07-25 16:06:28 -0500

    refactor: Update graph1 to use Nodes instead strings

--- notes/Grokking Algorithms Book/DijkstraAlgorithm.scala
+++ notes/Grokking Algorithms Book/DijkstraAlgorithm.scala
@@ -14,11 +14,12 @@ object DijkstraAlgorithm extends App {
 
   // 7.1
   val graph1 = Map(
-    "start" -> Map(Node("A") -> 5, Node("B") -> 2),
-    "A"     -> Map(Node("C") -> 4, Node("D") -> 2),
-    "B"     -> Map(Node("A") -> 8, Node("D") -> 7),
-    "C"     -> Map(Node("D") -> 6, Node("finish") -> 3),
-    "D"     -> Map(Node("finish") -> 1)
+    Node("start")  -> Map(Node("A") -> 5, Node("B") -> 2),
+    Node("A")      -> Map(Node("C") -> 4, Node("D") -> 2),
+    Node("B")      -> Map(Node("A") -> 8, Node("D") -> 7),
+    Node("C")      -> Map(Node("D") -> 6, Node("finish") -> 3),
+    Node("D")      -> Map(Node("finish") -> 1),
+    Node("finish") -> Map()
   )
 
   val costs1 = Map(
