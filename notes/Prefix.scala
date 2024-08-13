e88e38a7bb21c1b820cbd2e4a5c434d1381d1ac9
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-12 22:14:23 -0500

    feat: Add Prefix.scala file with unary prefix operator examples

---
+++ notes/Prefix.scala
@@ -0,0 +1,20 @@
+//> using scala "3.3.1"
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Reference: Scala Excercises
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+object Prefix extends App {
+
+  class Stereo {
+    // To make prefixes work, the method name must be unary_ followed by the operator
+    def unary_+ : String = "on"
+    def unary_- : String = "off"
+  }
+
+  val stereo = new Stereo
+  println(+stereo) // <- calls stereo.unary_+ so prints "on"
+  println(-stereo) // <- calls stereo.unary_- so prints "off"
+
+}
