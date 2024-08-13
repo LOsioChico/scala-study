b2b1422db99d0c7ab4fc36b66cdb986659193b92
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-12 22:12:52 -0500

    feat: Add linear search algorithm implementation

---
+++ notes/Algorithms/LinearSearch.scala
@@ -0,0 +1,29 @@
+//> using scala "3.3.1"
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * This linear search algorithm hasta a time complexity of O(n), since the
+ * running time time is directly proportional to the size of the input. If the
+ * input size is n, the linear search will nake n steps to complete.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Reference: Grokking Data Structures & Algorithms for Coding Interviews
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+object LinearSearch extends App {
+
+  def linearSearch(inputList: List[Int], target: Int) =
+    def iter(inputList: List[Int], target: Int, index: Int = 0): Int = 
+      inputList match
+        case head :: tail => 
+          if head == target then index
+          else iter(tail, target, index + 1)
+        case Nil => -1
+
+    iter(inputList, target)
+
+  val elements    = List[Int](3, 5, 7, 9, 11)
+  val searchValue = 7
+  println(linearSearch(elements, searchValue)) // <- 2
+}
