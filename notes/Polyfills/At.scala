10a20285a8a4142c6be308bba3a93ba4a2829ad2
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-09-08 17:54:51 -0500

    feat: Update At.scala to extend Seq prototype and retrieve element by index

--- notes/Polyfills/At.scala
+++ notes/Polyfills/At.scala
@@ -1,22 +1,22 @@
 //> using scala "3.3.1"
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Extends the Array prototype with a method that returns the element at the
+ * Extends the Seq prototype with a method that returns the element at the
  * specified index. If the index is negative, it is used as an offset from the
- * end of the array.
+ * end of the seq.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
 object At extends App {
-  extension [T](arr: Array[T]) {
+  extension [T](arr: Seq[T]) {
     def at(index: Int): T = {
       if (index < 0) arr(arr.length + index)
       else arr(index)
     }
   }
 
-  val arr = Array(1, 2, 3, 4, 5)
+  val arr = Seq(1, 2, 3, 4, 5)
 
   println(arr.at(-2)) // Output: 4
-  // The index -2 is used as an offset from the end of the array
+  // The index -2 is used as an offset from the end of the seq
 }
